7de8ef85b55b4349358c747f87030c113645ecb8
tokiclover <tokiclover@users.noreply.github.com>
2014-07-01 23:00:17 +0200

    Update and rename termcolors.sh to termcolors.bash

--- .scripts/termcolors.bash
+++ .scripts/termcolors.bash
@@ -1,14 +1,13 @@
 #!/bin/bash 
-# 
-#   This file echoes a bunch of color codes to the 
-#   terminal to demonstrate what's available.  Each 
-#   line is the color code of one forground color, 
-#   out of 17 (default + 16 escapes), followed by a 
-#   test use of that color on all nine background 
-#   colors (default + 8 escapes). 
-# 
 
-T='ABC'   # The test text 
+# This file echoes a bunch of color codes to the 
+# terminal to demonstrate what's available.  Each 
+# line is the color code of one forground color, 
+# out of 17 (default + 16 escapes), followed by a 
+# test use of that color on all nine background 
+# colors (default + 8 escapes). 
+
+T='ABC' # The test text 
 
 echo -e "\n                 40m     41m     42m     43m     44m     45m     46m     47m"; 
 
@@ -18,6 +17,9 @@ for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' '1;32m' '  33
   for BG in 40m 41m 42m 43m 44m 45m 46m 47m; 
     do echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m"; 
   done 
-  echo; 
+  echo
 done 
+
 exit 1
+
+# vim:fenc=utf-8:ci:pi:sts=0:sw=2:ts=2:

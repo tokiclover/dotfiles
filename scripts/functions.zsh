58b5885138331eaf6bcfcb3d2ee81f3e70b7fced
tokiclover <tokiclover@users.noreply.github.com>
2014-07-09 09:15:35 +0200

    scripts/functions.zsh: update helper functions

--- scripts/functions.zsh
+++ scripts/functions.zsh
@@ -1,12 +1,28 @@
-# $Id: $HOME/scripts/functions.zsh,v 2014/07/07 09:59:26 -tclover Exp $
+# $Id: $HOME/scripts/functions.zsh,v 2014/07/07 10:59:26 -tclover Exp $
+
+# @FUNCTION: error
+# @DESCRIPTION: hlper function, print message to stdout
+# @USAGE: <string>
+error() {
+	$LOG && logger -p $facility.err -t ${(%):-%1x}: $@
+	print -P "${(%):-%1x}: ${(%):-%1x}: %B%F{red}*%b%f $@"
+}
 
 # @FUNCTION: die
-# @DESCRIPTION: hlper function
+# @DESCRIPTION: hlper function, print message and exit
 # @USAGE: <string>
 function die() {
-  local _ret=$?
-  print -P " %F{red}*%f $@"
-  exit $_ret
+  local ret=$?
+  print -P "%F{red}*%f $@"
+  exit $ret
+}
+
+# @FUNCTION: info
+# @DESCRIPTION: hlper function, print message to stdout
+# @USAGE: <string>
+info() {
+	$LOG && logger -p $facility.err -t ${(%):-%1x}: $@
+	print -P "${(%):-%1x}: %B%F{green}*%b%f $@"
 }
 
 # vim:fenc=utf-8:ft=zsh:ci:pi:sts=0:sw=2:ts=2:

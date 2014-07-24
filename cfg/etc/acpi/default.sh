872a982bb66a955fca194d688d8d8f71ef8ce519
tokiclover <tokiclover@gmail.com>
2014-07-24 11:46:42 +0200

    cfg/etc/acpi/default.sh: clean up/update/fix, now work as expected

--- cfg/etc/acpi/default.sh
+++ cfg/etc/acpi/default.sh
@@ -1,88 +1,86 @@
 #!/bin/sh
-# $Id: /etc/acpi/default.sh, 2014/07/07 14:22:59 -tclover Exp $
+# $Id: /etc/acpi/default.sh, 2014/07/22 14:22:59 -tclover Exp $
+
+log() { logger -p daemon "ACPI: $*"; }
+uhd() { log "event unhandled: $*"; }
 
 set $*
-group=${1%%/*}
+group=${1%/*}
 action=${1#*/}
 device=$2
 id=$3
 value=$4
 
-[ -e /run/openrc/started/alsasound ] && alsa=true
-[ -e /run/openrc/started/oss ] && oss=true
+[ -e /run/openrc/started/alsasound ] && alsa=true || alsa=false
+[ -e /run/openrc/started/oss ]       && oss=true  || oss=false
 amixer="amixer -q set Master"
 ossmix="ossmix -- vmix0-outvol"
 
-log() {
-	logger -p daemon.notice "ACPI: $*"
-}
-
-unhandled() {
-	log "event unhandled: $*"
-}
-
-case "$group" in
+case $group in
 	ac_adapter)
-		case "$value" in
+		case $value in
 			*0) log "switching to power.bat power profile"
-					hprofile power.bat
-				;;
+				hprofile power.bat;;
 			*1) log "switching to power.adp power profile"
-					hprofile power.adp
-				;;
-			*)	unhandled $*;;
+				hprofile power.adp;;
+			*) uhd $*;;
 		esac
 		;;
 	battery)
-		case "$value" in
+		case $value in
 			*0) log "switching to power.adp power profile"
-					hprofile power.adp
-				;;
+				hprofile power.adp;;
 			*1) log "switching to power.adp power profile"
-						hprofile power.adp
-				;;
-			*) unhandled $*;;
+				hprofile power.adp;;
+			*) uhd $*;;
 		esac
 		;;
 	button)
-		case "$action" in
+		case $action in
 			lid)
 				case "$id" in
-					close) pm-suspend --quirk-none;;
-					*) unhandled $*;;
+					close) hibernate-ram;;
+					open) :;;
+					*) uhd $*;;
 				esac
 				;;
-			power) telinit 0;;
-			prog1) :;;
+			power) shutdown -H now;;
+			sleep) hibernate-ram;;
 			mute) 
 				$alsa && $amixer toggle;;
 			volumeup) 
 				$alsa && $amixer 3dB+
-				$oss && $ossmix +3
-				;;
+				$oss && $ossmix +3;;
 			volumedown) 
 				$alsa && $amixer 3dB-
-				$oss && $ossmix -3
-				;;
-			*)	unhandled $*;;
+				$oss && $ossmix -3;;
+			*) uhd $*;;
 		esac
 		;;
 	cd)
-		case "$action" in
+		case $action in
 			play) :;;
 			stop) :;;
 			prev) :;;
 			next) :;;
-			*) unhandled $*;;
+			*) uhd $*;;
 		esac
 		;;
 	jack)
-		case "$id" in
+		case $id in
 			*plug) :;;
-			*) unhandled $*;;
+			*) uhd $*;;
 		esac
 		;;
-	*)	unhandled $*;;
+	video)
+		case $action in
+			displayoff) :;;
+			*) uhd $*;;
+		esac
+		;;
+	*) uhd $*;;
 esac
 
-# vim:fenc=utf-8:ft=sh:ci:pi:sts=0:sw=2:ts=2:
+unset alsa oss amixer ossmix group action device id
+
+# vim:fenc=utf-8:ft=sh:ci:pi:sts=4:sw=4:ts=4:

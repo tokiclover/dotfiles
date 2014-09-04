01cf8947e06fb9ecd949c0a4104e2784023e1c8a
tokiclover <tokiclover@users.noreply.github.com>
2014-09-04 19:17:06 +0200

    term256colors.bash: major rewrite overhaul

--- scr/term256colors.bash
+++ scr/term256colors.bash
@@ -1,34 +1,60 @@
 #!/bin/bash
-# $Id: ~/scr/term256colors.bash, 2014/07/31 -tclover $
+#
+# $Id: term256colors.bash, 2014/07/31 -tclover      Exp $
+# $License: MIT (or 2-clause/new/simplified BSD)    Exp $
+#
+# $Original: http://frexx.de/xterm-256-notes/data/colortable16.sh
 
-# Original: http://frexx.de/xterm-256-notes/
-#           http://frexx.de/xterm-256-notes/data/colortable16.sh
-# Modified by Aaron Griffin
-# and further by Kazuo Teramoto; and further tclover
+printline() {
+    echo "     ----------------------------------------------------------------------------"
+}
 
-FGNAMES=(' black ' '  red  ' ' green ' ' yellow' '  blue ' 'magenta' '  cyan ' ' white ')
-BGNAMES=('DFT' 'BLK' 'RED' 'GRN' 'YEL' 'BLU' 'MAG' 'CYN' 'WHT')
+declare BO FO C E="\e["
+declare B="${E}48;5" F="38;5" R="${E}m"
 
-echo "     ----------------------------------------------------------------------------"
-for b in $(seq 0 8); do
-    if [ "$b" -gt 0 ]; then
-        bg=$(($b+39))
-    fi
-    echo -en "\033[0m ${BGNAMES[$b]} : "
-    for f in $(seq 0 7); do
-        echo -en "\033[${bg}m\033[$(($f+30))m ${FGNAMES[$f]} "
-    done
-    echo -en "\033[0m :"
-    echo -en "\033[0m\n\033[0m     : "
-    for f in $(seq 0 7); do
-        echo -en "\033[${bg}m\033[1;$(($f+30))m ${FGNAMES[$f]} "
-    done
-    echo -en "\033[0m :"
-    echo -e "\033[0m"
-    if [ "$b" -lt 8 ]; then
-        echo "     ----------------------------------------------------------------------------"
-    fi
+C=$(tput colors)
+[[ $C -lt 256 ]] && echo "256 colors not supported" >&2 && exit 1
+
+declare -a CLR
+CLR=(DFT BLK RED GRN YEL BLU MAG CYN WHT)
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -b|--bg-offset)
+            BO=$2
+            shift 2;;
+        -f|--fo-offset)
+            FO=$2
+            shift 2;;
+        *)
+            echo "unrecognized option" >&2
+            echo "usage: ${0##*/} [-b|--bg-offset <offset>] [-f|--fg-offset <offset>]"
+            exit 1;;
+    esac
 done
-echo "     ----------------------------------------------------------------------------"
+
+[[ -n "$BO" ]] && ( (( $BO <= (256-8) )) || BO=0 )
+[[ -n "$FO" ]] && ( (( $FO <= (256-8) )) || FO=0 )
+
+printline
+for (( b=0; i<8; i++ )); do
+	echo -en "$R ${CLR[$b]} : "
+	for (( f=0; f<8; f++ )); do
+		echo -en "${B};$(($b+$BO))m${E}${F};$(($f+$FO))m ${CLR[$(($f+1))]} "
+	done
+
+	echo -en "$R :"
+	echo -en "$R\n$R     : "
+	for (( f=0; f<8; f++ )); do
+		echo -en "${B};$(($b+$BO))m${E}1;${F};$(($f+$FO))m ${CLR[$(($f+1))]} "
+	done
+
+	echo -en "$R :"
+	echo -e "$R"
+	printline
+done
+printline
+
+unset B CLR E F R
 
 # vim:fenc=utf-8:ci:pi:sts=0:sw=2:ts=2:

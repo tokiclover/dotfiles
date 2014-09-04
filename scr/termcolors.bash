17e0f6e4b334c295146d156cad2bf05f6348e151
tokiclover <tokiclover@users.noreply.github.com>
2014-09-04 19:54:43 +0200

    termcolors.bash: major rewrite overhaul

--- scr/termcolors.bash
+++ scr/termcolors.bash
@@ -1,26 +1,30 @@
-#!/bin/bash 
-# $Id: ~/scr/termcolors.bash, 2014/07/31 $
+#!/bin/bash
+#
+# $Id: termcolors.bash, 2014/07/31 -tclover Exp $
+# $License: MIT (or 2-clause/new/simplified BSD) Exp $
 
-# This file echoes a bunch of color codes to the 
-# terminal to demonstrate what's available.  Each 
-# line is the color code of one forground color, 
-# out of 17 (default + 16 escapes), followed by a 
-# test use of that color on all nine background 
-# colors (default + 8 escapes). 
+declare C E="\e[" T=ABC
+declare B="${E}48;5" F="38;5" R="${E}0m"
+declare -a CLR
+CLR=(BLK RED GRN YEL BLU MAG CYN WHT)
 
-T='ABC' # The test text 
+echo -en "\n             "
+for (( i=0; i<8; i++ )); do
+	echo -en "${CLR[$i]}     "
+done
+echo
 
-echo -e "\n                 40m     41m     42m     43m     44m     45m     46m     47m"; 
+printline() {
+	echo -en " ${CLR[$i]} ${E}${1}3${i}  ${T}  "
+	for (( j=0; j<8; j++ )); do
+		echo -en " ${E}${1}3${i}${E}${1}4${j}  ${T}  ${R}"
+	done
+	echo
+}
 
-for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' '  36m' '1;36m' '  37m' '1;37m'; 
-  do FG=${FGs// /} 
-  echo -en " $FGs \033[$FG  $T  " 
-  for BG in 40m 41m 42m 43m 44m 45m 46m 47m; 
-    do echo -en "$EINS \033[$FG\033[$BG  $T  \033[0m"; 
-  done 
-  echo
-done 
-
-exit 1
-
-# vim:fenc=utf-8:ci:pi:sts=0:sw=2:ts=2:
+for (( i=0; i<8; i++ )); do
+	printline
+	printline "1;"
+done
+	
+# vim:fenc=utf-8:ci:pi:sts=0:sw=4:ts=4:

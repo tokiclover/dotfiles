6bed9ed414377bce30877f6cc85d71b808298410
tokiclover <tokiclover@gmail.com>
2014-09-26 11:26:34 +0200

    .zprezto/customs/key-bindings.zsh: update

--- .zprezto/customs/key-bindings.zsh
+++ .zprezto/customs/key-bindings.zsh
@@ -4,8 +4,13 @@ if zstyle -t ':prezto:module:editor' key-bindings 'emacs' 'vi'; then
     bindkey -M "$keymap" "\EOq" end-of-line
     bindkey -M "$keymap" "\EOt" backward-char
     bindkey -M "$keymap" "\EOv" forward-char
-    bindkey -M "$keymap" "\EOx" up-line-or-history
-    bindkey -M "$keymap" "\EOr" down-line-or-history
+    bindkey -M "$keymap" "\EOy" up-line-or-history
+    bindkey -M "$keymap" "\EOs" down-line-or-history
+
+    bindkey -M "$keymap" "\EOd" backward-word
+    bindkey -M "$keymap" "\EOc" forward-word
+    bindkey -M "$keymap" "\EOa" up-line-or-history
+    bindkey -M "$keymap" "\EOb" down-line-or-history
   done
 fi
 unset keymap

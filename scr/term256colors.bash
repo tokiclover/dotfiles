9e819d6e02cad79228a89d5f78edd39dedfe61b7
tokiclover <tokiclover@gmail.com>
2014-09-27 08:27:20 +0200

    scr/term256colors.bash: update

--- scr/term256colors.bash
+++ scr/term256colors.bash
@@ -2,7 +2,7 @@
 #
 # $Header: term256colors.bash, 2014/09/22 -tclover          Exp $
 # $License: MIT (or 2-clause/new/simplified BSD)            Exp $
-# $Screenhots: https://imgur.com/ZOFawH8.png                Exp $
+# $Screenhots: https://imgur.com/EqE6iOz.png                Exp $
 #
 # $Original: http://frexx.de/xterm-256-notes/data/colortable16.sh
 
@@ -10,11 +10,10 @@ printline() {
     echo "     >------------------------------------------<"
 }
 
-declare BO=0 FO=0 C E="\e["
+declare BO=0 FO=0 E="\e["
 declare B="${E}48;5" F="38;5" R="${E}0m"
 
-C=$(tput colors)
-[[ $C -lt 256 ]] && echo "256 colors not supported" >&2 && exit 1
+(( $(tput colors) < 256 )) && echo "256 colors not supported" >&2 && exit 1
 
 declare -a CLR
 CLR=(DFT BLK RED GRN YEL BLU MAG CYN WHT)
@@ -34,25 +33,20 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-if [[ -n "$BO" ]]; then
-	(( $BO <= (256-8) )) || BO=0
-fi
-if [[ -n "$FO" ]]; then
-	(( $FO <= (256-8) )) || FO=0
-fi
+(( $BO <= (256-8) )) || BO=0
+(( $FO <= (256-8) )) || FO=0
 
 printline
 for (( b=0; i<8; i++ )); do
 	echo -en "$R ${CLR[$b]} : "
-	bo=$(($b+$BO))
 	for (( f=0; f<8; f++ )); do
-		echo -en "${B};$(($f+$FO))m${E}${F};${fo}m ${CLR[$(($f+1))]} "
+		echo -en "${B};$(($b+$BO))m${E}${F};$(($f+$FO))m ${CLR[$(($f+1))]} "
 	done
 
 	echo -en "$R :"
 	echo -en "$R\n$R     : "
 	for (( f=0; f<8; f++ )); do
-		echo -en "${B};$(($f+$FO))m${E}1;${F};${fo}m ${CLR[$(($fo+1))]} "
+		echo -en "${B};$(($f+$BO))m${E}1;${F};$(($f+$FO))m ${CLR[$(($f+1))]} "
 	done
 
 	echo -en "$R :"

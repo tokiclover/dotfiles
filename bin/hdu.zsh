ec53acb989bf63e257350e2cfa32e0a3f07194ad
tokiclover <tokiclover@gmail.com>
2014-09-29 09:29:11 +0200

    bin/hdu.zsh: clean up

--- bin/hdu.zsh
+++ bin/hdu.zsh
@@ -1,37 +1,48 @@
 #!/bin/zsh
 #
-# $Id: hdu.zsh,v 1.1 2014/08/31 23:00:55 -tclover Exp $
-# $License: MIT (or 2-clause/new/simplified BSD)  Exp $
+# $Header: hdu.zsh,v 1.1 2014/08/31 23:00:55 -tclover Exp $
+# $License: MIT (or 2-clause/new/simplified BSD)      Exp $
 #
-usage() {
+
+function usage {
   cat <<-EOF
   usage: ${(%):-%1x} [options] <files>
-  -d|-date <date>      old date or regex to replace by a new date
-  -a|-author <author>  include <file> or file
-  -u|-usage            print this help/usage and exit
+  -d|-date<date>      old date or regex to replace by a new date
+  -a|-author<author>  include <file> or file
+  -h|-help            print this help/usage and exit
 EOF
 exit $?
 }
-error() { print -P "%B%F{red}*%b%f $@" }
-die() {
-	error $@
-	exit 1
+
+function error {
+	print -P "%B%F{red}%1x%b%f: $argv"
 }
-alias die='die "%F{yellow}%1x:%U${(%):-%I}%u:%f" $@'
+
+function die {
+	local ret=$?
+	error $@
+	exit $ret
+}
 
 zmodload zsh/zutil
-zparseopts -E -D -K -A opts a: author: d: date: u usage || usage
+zparseopts -E -D -K -A opts a: author: d: date: h help || usage
 
-if [[ -n ${(k)opts[-u]} ]] || [[ -n ${(k)opts[-usage]} ]] { usage }
+(( $+opts[-h] )) || (( $+opts[-help] )) && usage
+
 if [[ -z ${opts[*]} ]] { typeset -A opts }
 :	${opts[-date]:=${opts[-d]:-$(date +%Y)}}
 : 	${opts[-newd]:=$(date +%Y/%m/%d\ %T)}
+
 if [[ -n ${opts[-author]:-${opts[-a]}} ]] {
 	opts[-author]="-e s,-\ .*([a-z][A-Z]).*\ Exp,-\ ${opts[-author]:-${opts[-a]}}\ Exp,g"
 }
+
 for file ($*)
 	sed -e "s,${opts[-date]}.*-,${opts[-newd]} -,g" ${opts[-author]} \
 		-i ${file} || die "${file}: failed to update file"
+
 unset -v opts
 
+#
 # vim:fenc=utf-8:ci:pi:sts=0:sw=4:ts=4:
+#

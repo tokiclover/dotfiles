96463b76ef047089aecb58d88f373e3090fbed3a
tokiclover <tokiclover@gmail.com>
2014-10-05 09:39:13 +0200

    .zprezto/customs/key-bindings.zsh: update

--- .zprezto/customs/key-bindings.zsh
+++ .zprezto/customs/key-bindings.zsh
@@ -5,10 +5,15 @@
 # key-bindings.zsh
 #
 
+bindkey -s '\EOa' '\C-[A'
+bindkey -s '\EOb' '\C-[B'
+bindkey -s '\EOc' '\C-[C'
+bindkey -s '\EOd' '\C-[D'
+
 autoload -Uz copy-earlier-word
 zle -N copy-earlier-word
 
-for keymap in 'emacs' 'viins' 'vicmd'; do
+for keymap in 'emacs' 'viins'; do
 	bindkey -M "$keymap" "\EOw" beginning-of-line
 	bindkey -M "$keymap" "\EOq" end-of-line
 	bindkey -M "$keymap" "\EOt" backward-char
@@ -28,10 +33,10 @@ for keymap in 'emacs' 'viins' 'vicmd'; do
 	bindkey -M "$keymap" "\E[b" backward-kill-line
 	bindkey -M "$keymap" "\E[c" backward-kill-word
 	bindkey -M "$keymap" "\E[d" kill-word
-	bindkey -M "$keymap" "\EOa" undo
-	bindkey -M "$keymap" "\EOb" redo
-	bindkey -M "$keymap" "\EOc" forward-word
-	bindkey -M "$keymap" "\EOd" backward-word
+	bindkey -M "$keymap" "\C[A" undo
+	bindkey -M "$keymap" "\C[B" redo
+	bindkey -M "$keymap" "\C[C" forward-word
+	bindkey -M "$keymap" "\C[D" backward-word
 done
 unset keymap
 

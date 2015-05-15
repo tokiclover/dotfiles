53d0fba9695936cbcf76593d102d8c99c6ed731f
tclover <tokiclover@gmail.com>
2015-05-15 14:49:00 +0200

    lib/helpers.bash: Major refactorization and splitted to helpers

--- lib/helpers.bash
+++ lib/helpers.bash
@@ -1,117 +1,83 @@
 #
 # $Header: ${HOME}/helpers.bash                         Exp $
-# $Author: (c) 2012-015 -tclover <tokiclover@gmail.com> Exp $
+# $Author: (c) 2012-15 -tclover <tokiclover@gmail.com>  Exp $
 # $License: MIT (or 2-clause/new/simplified BSD)        Exp $
-# $Version: 2015/05/14 21:09:26                         Exp $
+# $Version: 2015/05/15 21:09:26                         Exp $
 #
 
 #
 # @FUNCTION: little helpter to retrieve Kernel Module Parameters
 #
-function kmod-pa {
-	local c d line m mc mod md de n=/dev/null o
-	c=$(tput op) o=$(echo -en "\n$(tput setaf 2)-*- $(tput op)")
-	if [[ -n "$*" ]]
-	then
-		mod=($*)
+function mod-info {
+	local dir line conf mod{,s} info de null=/dev/null
+
+	if [[ -n "$*" ]]; then
+		mods=($*)
 	else
-		while read line
-		do
-			mod+=( ${line%% *})
+		while read line; do
+			mods+=( ${line%% *})
 		done </proc/modules
 	fi
-	for m in ${mod[@]}
-	do
-		md=/sys/module/$m/parameters
-		[[ ! -d $md ]] && continue
-		d=$(modinfo -d $m 2>$n | tr '\n' '\t')
-		echo -en "$o$m$c ${d:+:$d}"
-		echo
-		pushd $md >$n 2>&1
-		for mc in *
-		do
-			de=$(modinfo -p $m 2>$n | grep ^$mc 2>$n | sed "s/^$mc=//" 2>$n)
-			echo -en "\t$mc=$(cat $mc 2>$n) ${de:+ -$de}"
-			echo
+	for mod in "${mods[@]}"; do
+		dir=/sys/module/$mod/parameters
+		[[ -d $dir ]] || continue
+		info="$(modinfo -d $mod 2>$null)"
+		echo -e "$mod${color[default]} :$(echo "$info" | tr '\n' '\t')"
+
+		pushd $dir >$null 2>&1
+		for conf in *; do
+			echo -e "\t$conf=$(< $conf 2>$null) -$(echo "$info" | sed "/^$conf/s/^$conf=//" 2>$null)"
 		done
-		popd >$n 2>&1
+		popd >$null 2>&1
 	done
 }
 
 #
 # @FUNCTION: colorful helper to retrieve Kernel Module Parameters
 #
-function kmod-pc {
-	local green yellow cyan reset
-	if [[ "$(tput colors)" -ge 8 ]]
-	then
-		green="\e[1;32m"
-		yellow="\e[1;33m"
-		cyan="\e[1;36m"
-		reset="\e[0m"
-	fi
-	newline='
+function mod-info-color {
+	local line conf dir mod{,s} info null=/dev/null newline='
 '
-
-	local d line m mc md mod n=/dev/null
-	if [[ -n "$*" ]]
-	then
-		mod=($*)
+	if [[ -n "$*" ]]; then
+		mods=($*)
 	else
-		while read line
-		do
-			mod+=( ${line%% *})
+		while read line; do
+			mods+=( ${line%% *})
 		done </proc/modules
 	fi
-	for m in ${mod[@]}
-	do
-		md=/sys/module/$m/parameters
-		[[ ! -d $md ]] && continue
-		d="$(modinfo -d $m 2>$n | tr '\n' '\t')"
-		echo -en "$green$m$reset"
-		[[ ${#d} -gt 0 ]] && echo -n " - $d"
-		echo
-		declare pnames=() pdescs=() pvals=()
-		local add_desc=false p pdesc pname
-		while IFS="$newline" read p
-		do
-			if [[ $p =~ ^[[:space:]] ]]
-			then
-				pdesc+="$newline	$p"
+	for mod in ${mods[@]}; do
+		dir=/sys/module/$mod/parameters
+		[[ -d $dir ]] || continue
+		info="$(modinfo -d $mod 2>$null | tr '\n' '\t')"
+		echo -en "${fg[2]}$mod${color[none]}"
+		(( ${#info} >= 0 )) && echo -e " - $info"
+
+		declare -a names descs vals
+		local add_desc=false desc name IFS="$newline"
+		
+		while read line; do
+			if [[ "$line" =~ ^[[:space:]] ]]; then
+				desc+="$newline	$line"
 			else
-				$add_desc && pdescs+=("$pdesc")
-				pname="${p%%:*}"
-				pnames+=("$pname")
-				pdesc=("	${p#*:}")
-				pvals+=("$(cat $md/$pname 2>$n)")
+				$add_desc && descs+=("$desc")
+				name="${line%%:*}"
+				names+=("$name")
+				desc=("	${line#*:}")
+				vals+=("$(< $dir/$name 2>$null)")
 			fi
 			add_desc=true
-		done < <(modinfo -p $m 2>$n)
-		$add_desc && pdescs+=("$pdesc")
-		for ((i=0; i<${#pnames[@]}; i++))
-		do
-			[[ -z ${pnames[i]} ]] && continue
-			printf "\t$cyan%s$reset = $yellow%s$reset\n%s\n" \
-			${pnames[i]} \
-			"${pvals[i]}" \
-			"${pdescs[i]}"
+		done < <(modinfo -p $mod 2>$null)
+		
+		$add_desc && descs+=("$desc")
+		for (( i=0; i<${#names[@]}; i++ )); do
+			(( "${#names[i]}" > 0 )) || continue
+			printf "\t${fg[6]}%s${color[none]} = ${fg[3]}%s${color[none]}\n%s\n" \
+			${names[i]} \
+			"${vals[i]}" \
+			"${descs[i]}"
 		done
 		echo
 	done
-}
-
-#
-# @FUNCTION: generate a random password using openssl to stdout
-#
-function genpwd {
-	openssl rand -base64 48
-}
-#
-# @FUNCION: simple xev key code
-#
-function xev-key-code {
-	xev | grep -A2 --line-buffered '^KeyRelease' | \
-	sed -nre '/keycode /s/^.*keycode ([0-9]*).* (.*, (.*)).*$/\1 \2/p'
 }
 
 #

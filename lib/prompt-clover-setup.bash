65b3f6d74a5000aef74ca3c27ec8224e7a553a5e
tclover <tokiclover@gmail.com>
2015-06-26 12:32:03 +0200

    lib/prompt-clover-setup.bash: Escape no-printing charcacters (important fix)

--- lib/prompt-clover-setup.bash
+++ lib/prompt-clover-setup.bash
@@ -2,7 +2,7 @@
 # $Header: ${HOME}/lib/promp-clover-setup.bash          Exp $
 # $Author: (c) 2012-15 -tclover <tokiclover@gmail.com>  Exp $
 # $License: 2-clause/new/simplified BSD                 Exp $
-# $Version: 1.0 2015/05/05 21:09:26                     Exp $
+# $Version: 1.0 2015/06/25 21:09:26                     Exp $
 #
 
 # color: http://www.calmar.ws/vim/256-xterm-24bit-rgb-color-chart.html
@@ -12,8 +12,9 @@ function prompt-clover-setup {
 		(0) uc="${fg[1]}";;
 		(*) uc="${fg[2]}";;
 	esac
-	PS1="${color[bold]}${fg[5]}-${fg[4]}(${uc}\$·${fg[5]}\h:${TTY}${fg[4]}·\D{%m/%d}·${fg[5]}\A${fg[4]})-${fg[2]}»${color[none]} "
-	PS2="${color[bold]}${fg[5]}-${fg[2]}» ${color[none]}"
+	PS1="\[${color[bold]}${fg[5]}\]-\[${fg[4]}\](\[${uc}\]\$·\[${fg[5]}\]\h:${TTY}\[${fg[4]}\]·\D{%m/%d}·\[${fg[5]}\]\A\[${fg[4]}\])-\[${fg[2]}\]»\[${color[none]}\] "
+	PS2="\[${color[bold]}${fg[5]}\]-\[${fg[2]}\]» \[${color[none]}\]"
+	PROMPT_DIRTRIM=3
 	TITLEBAR="\$:\w"
 }
 PROMPT_COMMAND=prompt-clover-setup
